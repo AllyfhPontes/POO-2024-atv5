19cef372963ab371c2f4f8ec2ed1a9b57663c4aa
ALLYFH FERNANDO DA SILVA PONTES <20231041110028@ifrn.local>
2024-11-13 11:07:27 -0300

    correção do código vendas e organização do repositório.

---
+++ atvv9/teste_vendas.py
@@ -0,0 +1,59 @@
+from Produto import Produto
+from vendas import Venda
+
+data = input("Digite a data da venda (formato: DD/MM/AAAA): ")
+venda = Venda(data)
+opcao = "0"
+
+while opcao != "5":
+    print("\nMenu:")
+    print("0. Carregar todos os produtos")
+    print("1. Adicionar Produto")
+    print("2. Remover Produto")
+    print("3. Listar Produtos e Mostrar Total")
+    print("4. Salvar Venda em JSON")
+    print("5. Sair")
+    
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "0":
+        print("Carregando produtos do arquivo JSON:")
+        venda.carregar_venda_json()
+    
+    elif opcao == "1":
+        nome = input("Nome do Produto: ")
+
+        while True:
+            preco = input("Preço do Produto: ").replace(',', '.')
+            try:
+                preco = float(preco)
+                break
+            except ValueError:
+                print("Preço inválido. Por favor, insira um valor numérico.")
+
+        while True:
+            quantidade = input("Quantidade em Estoque: ")
+            if quantidade.isdigit():
+                quantidade = int(quantidade)
+                break
+            else:
+                print("Quantidade inválida. Por favor, insira um número inteiro.")
+
+        produto = Produto(nome, preco, quantidade)
+        venda.get_produtos().append(produto)
+  
+    elif opcao == "2":
+        nome = input("Nome do Produto a remover: ")
+        venda.removerProduto(nome)
+
+    elif opcao == "3":
+        venda.listarProdutos()
+
+    elif opcao == "4":
+        venda.salvar_em_json("venda.json")
+
+    elif opcao == "5":
+        print("Saindo...")
+
+    else:
+        print("Opção inválida, tente novamente.")
---
+++ atvv9/vendas.py
@@ -0,0 +1,79 @@
+import json
+from Produto import Produto
+
+class Venda:
+    def __init__(self, dataVenda):
+        self.__produtos = []
+        self.__dataVenda = dataVenda
+        self.__total = 0.0
+
+    def get_produtos(self):
+        return self.__produtos
+        
+    def get_dataVenda(self):
+        return self.__dataVenda
+
+    def get_total(self):
+        return self.__total
+
+    def set_dataVenda(self, dataVenda):
+        self.__dataVenda = dataVenda
+
+    def calcularTotal(self):
+        self.__total = sum(produto.get_preco() * produto.get_quantidade() for produto in self.__produtos)
+        return self.__total
+
+    def removerProduto(self, nome):
+        for produto in self.__produtos:
+            if produto.get_nome() == nome:
+                self.__produtos.remove(produto)
+                print(f"Produto {nome} removido.")
+                return
+        print(f"Produto {nome} não encontrado.")
+
+    def listarProdutos(self):
+        if not self.__produtos:
+            print("Nenhum produto na venda.")
+        else:
+            print(f"\nProdutos na Venda do dia {self.__dataVenda}:")
+            for produto in self.__produtos:
+                print(f"Nome: {produto.get_nome()}, Preço: R${produto.get_preco():.2f}, Quantidade: {produto.get_quantidade()}")
+            print(f"Total: R${self.calcularTotal():.2f}")
+
+    def to_dict(self):
+        return {
+            "dataVenda": self.__dataVenda,
+            "produtos": [
+                {"nome": produto.get_nome(), "preco": produto.get_preco(), "quantidade": produto.get_quantidade()}
+                for produto in self.__produtos
+            ],
+            "total": self.calcularTotal()
+        }
+
+    def salvar_em_json(self, arquivo="venda.json"):
+        dados_venda = self.to_dict()
+        with open(arquivo, 'w') as f:
+            json.dump(dados_venda, f, indent=4)
+       
+        print(f"Venda salva no arquivo '{arquivo}' com sucesso!")
+
+    def carregar_venda_json(self, arquivo="venda.json"):
+        try:
+            with open(arquivo, 'r') as f:
+                dados = json.load(f)
+            self.__dataVenda = dados["dataVenda"]
+            self.__produtos = [
+                Produto(prod["nome"], prod["preco"], prod["quantidade"])
+                for prod in dados["produtos"]
+            ]
+        except FileNotFoundError:
+            print(f"O arquivo {arquivo} não foi encontrado.")
+        except json.JSONDecodeError:
+            print(f"O arquivo {arquivo} não está no formato JSON correto.")
+       
+
+           
+        print("Produtos carregados do arquivo JSON:")
+        self.listarProdutos()
+       
+        print(f"O arquivo '{arquivo}' não foi encontrado.")
